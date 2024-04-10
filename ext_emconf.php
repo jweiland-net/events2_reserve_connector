859da46f72205eb87f76df0a1bb68784f4f33cf4
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-04-10 16:57:10 +0200

    Added emconf configuration

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Events2 Reserve Connector',
+    'description' => 'This extension provides extended functionalities for integrating Events2 and Reserve extension.',
+    'category' => 'plugin',
+    'author' => 'Stefan Froemken, Hoja Mustaffa Abdul Latheef',
+    'author_email' => 'projects@jweiland.net',
+    'author_company' => 'jweiland.net',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.8-12.4.99',
+            'events2' => '9.0.0-0.0.0',
+        ],
+        'conflicts' => [
+        ],
+    ],
+];
