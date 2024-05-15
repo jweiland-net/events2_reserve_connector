d1be396b3be2ea3e4bcb06c48c88aa9795b9621e
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-05-15 20:19:18 +0200

    Fixed formating issues

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use MyVendor\MyExtension\Domain\Model\MyExtendedModel;
-
 return [
     \JWeiland\Events2ReserveConnector\Domain\Model\Location::class => [
         'tableName' => 'tx_events2_domain_model_location',
