2bcb444bfac300ccac759528b85240e35779e0c2
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-05-31 15:06:01 +0200

    [TASK] Removed un-relavant fields which are not required

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,11 +3,8 @@
 #
 CREATE TABLE tx_events2_domain_model_event
 (
-	release_date     int(11) DEFAULT '0',
-	social_teaser    varchar(255) DEFAULT '',
-	theater_details  text,
-	deadline         int(11) DEFAULT '0' NOT NULL,
-	reserve_period   int(11) DEFAULT '0' NOT NULL
+	deadline       int(11) DEFAULT '0' NOT NULL,
+	reserve_period int(11) DEFAULT '0' NOT NULL
 );
 
 #
